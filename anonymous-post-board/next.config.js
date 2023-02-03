087230dcc48ee8aeb16ab1e7f53a6bd9bd524aaf
Martin Miglio <marmig0404@gmail.com>
2023-02-02 21:54:05 -0500

    getting ready for deploy

--- anonymous-post-board/next.config.js
+++ anonymous-post-board/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  reactStrictMode: true,
-}
+  assetPrefix: isProd ? "/anon-post-board" : undefined,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
