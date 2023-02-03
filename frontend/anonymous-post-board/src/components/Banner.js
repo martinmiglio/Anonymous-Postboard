93a203293ac25f575c13f3ebb36977df5747e0a8
Martin Miglio <marmig0404@gmail.com>
2023-02-02 21:35:39 -0500

    page update more responsive, more colorful generate demo posts add new post page

--- frontend/anonymous-post-board/src/components/Banner.js
+++ frontend/anonymous-post-board/src/components/Banner.js
@@ -1,45 +1,74 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSync } from "@fortawesome/free-solid-svg-icons";
+import { faSync, faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 
-const Banner = ({ onRefresh }) => {
+const Banner = ({ onRefresh, onNewPost }) => {
   const [loading, setLoading] = useState(false);
 
   const handleRefresh = () => {
     setLoading(true);
     onRefresh().then(() => setLoading(false));
   };
-  
+
+  const handleNewPost = () => {
+    onNewPost();
+  };
+
   return (
     <div
       style={{
+        background: "linear-gradient(to bottom, #b3ffb3, #00cc99)",
         display: "flex",
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
-        backgroundColor: "darkgray",
         padding: "10px",
         width: "100%",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        height: "50px",
+        zIndex: 1,
       }}
     >
-      <h1>Anonymous Post Board</h1>
-      <button
-        id="refreshButton"
-        onClick={handleRefresh}
-        style={{
-          background: "none",
-          border: "none",
-          cursor: "pointer",
-          outline: "none",
-          padding: "8px",
-        }}
-      >
-        {loading ? (
-          <FontAwesomeIcon icon={faSync} spin />
-        ) : (
-          <FontAwesomeIcon icon={faSync} />
-        )}
-      </button>
+      <h1 style={{ fontSize: "24px" }}>Anonymous Post Board</h1>
+      <div style={{ width: "fit-content" }}>
+        {onNewPost ? (
+          <button
+            id="newPostButton"
+            onClick={handleNewPost}
+            style={{
+              background: "none",
+              border: "none",
+              cursor: "pointer",
+              outline: "none",
+              padding: "8px",
+            }}
+          >
+            <FontAwesomeIcon icon={faPlusCircle} />
+          </button>
+        ) : null}
+        {onRefresh ? (
+          <button
+            id="refreshButton"
+            onClick={handleRefresh}
+            style={{
+              background: "none",
+              border: "none",
+              cursor: "pointer",
+              outline: "none",
+              padding: "8px",
+            }}
+          >
+            {loading ? (
+              <FontAwesomeIcon icon={faSync} spin />
+            ) : (
+              <FontAwesomeIcon icon={faSync} />
+            )}
+          </button>
+        ) : null}
+      </div>
     </div>
   );
 };
--- frontend/anonymous-post-board/src/pages/index.js
+++ frontend/anonymous-post-board/src/pages/index.js
@@ -1,46 +1,34 @@
+import React, { useEffect, useState } from "react";
 import styles from "@/styles/Home.module.css";
 import PostList from "@/components/PostList.js";
 import Banner from "@/components/Banner.js";
-
-const onRefresh = () => {
-  console.log("refreshing...");
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, 1000);
-  });
-};
+import getPosts from "./api/get_posts";
 
 export default function Home() {
-  const posts = [
-    {
-      id: 1,
-      content: "This is my first post",
-      votes: 10,
-      replies: [
-        {
-          id: 1,
-          content: "This is a reply to the first post",
-          votes: 5,
-        },
-        {
-          id: 2,
-          content: "This is another reply to the first post",
-          votes: 3,
-        },
-      ],
-    },
-    {
-      id: 2,
-      content:
-        "This is my second post. It should be long enough to wrap to the next line. I hope it works.",
-      votes: 20,
-      replies: [],
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+
+  const onRefresh = async () => {
+    console.log("refreshing...");
+    // get the latest posts
+    const posts = await getPosts();
+    setPosts(posts);
+  };
+
+  const onNewPost = () => {
+    console.log("new post");
+    // forwards to the new post page
+    location.href = "new_post";
+  };
+
+  useEffect(() => {
+    getPosts().then((posts) => {
+      setPosts(posts);
+    });
+  }, []);
+
   return (
     <div className={styles.centerpane}>
-      <Banner onRefresh={onRefresh}/>
+      <Banner onRefresh={onRefresh} onNewPost={onNewPost} />
       <div className={styles.content}>
         <PostList posts={posts} />
       </div>
