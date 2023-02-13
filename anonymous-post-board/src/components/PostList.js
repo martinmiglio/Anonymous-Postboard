05ec3a33408434c1c968b73ff1b9b5d07ac329d9
Martin Miglio <marmig0404@gmail.com>
2023-02-13 05:41:22 -0500

    implement infinite scroll

--- anonymous-post-board/src/components/PostList.js
+++ anonymous-post-board/src/components/PostList.js
@@ -1,13 +1,27 @@
 import React from "react";
+import InfiniteScroll from "react-infinite-scroller";
+
 import Post from "./Post";
 
-function PostList({ posts }) {
+function PostList({ posts, loadMorePosts }) {
   console.log(posts);
   return (
     <div>
-      {posts.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={loadMorePosts}
+        hasMore={true}
+        loader={
+          <div style={{ textAlign: "center" }} key={0}>
+            Loading ...
+          </div>
+        }
+        useWindow={false}
+      >
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </InfiniteScroll>
     </div>
   );
 }
