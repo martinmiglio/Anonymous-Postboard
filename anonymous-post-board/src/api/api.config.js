7105e153b33c1e130b21c1da537c83c1d972b37c
Martin Miglio <marmig0404@gmail.com>
2023-02-13 13:38:30 -0500

    update api calls to fetch N posts at a time

--- anonymous-post-board/src/api/api.config.js
+++ anonymous-post-board/src/api/api.config.js
@@ -1,1 +1,1 @@
-export const API_URL = "https://api.postboard.martinmiglio.dev/posts";
+export const API_URL = "https://api.postboard.martinmiglio.dev/";
--- anonymous-post-board/src/api/posts.js
+++ anonymous-post-board/src/api/posts.js
@@ -3,12 +3,14 @@ import axios from "axios";
 import { API_URL } from "./api.config.js";
 
 export async function getLatestPosts() {
-  const response = await axios.get(`${API_URL}?latest`);
+  const response = await axios.get(`${API_URL}/posts?latest=true`);
+  console.log(`fetched ${response.data.length} posts to start`);
   return response.data;
 }
 
 export async function fetchMorePosts(after) {
   // fetch the next set of posts after the specified ID
-  const response = await axios.get(`${API_URL}?after=${after}`);
+  const response = await axios.get(`${API_URL}/posts?after=${after}&count=5`);
+  console.log(`fetched ${response.data.length} more posts`);
   return response.data;
 }
