6c1e0d134d1c65fc0c38e07455a5ca1dbf7157b4
Martin Miglio <marmig0404@gmail.com>
2023-03-01 21:06:40 -0500

    dont update votes when firstLoad changes

--- anonymous-post-board/src/components/Post.js
+++ anonymous-post-board/src/components/Post.js
@@ -24,7 +24,7 @@ function Post({ post }) {
     PostAPI.changeVotes(post.id, votes).then((res) => {
       console.log(`Updated post ${res.id} vote count to ${res.votes} `);
     });
-  }, [votes, firstLoad, post.id]);
+  }, [votes, post.id]);
 
   useEffect(() => {
     // if status is undefined, remove the item from localStorage
