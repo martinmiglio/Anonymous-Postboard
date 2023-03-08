10d3c746aa3adcfc23c6b9bce2786d8e63447839
Martin Miglio <marmig0404@gmail.com>
2023-03-07 19:55:00 -0500

    update retries and timeouts for production

--- anonymous-post-board/src/api/api.config.js
+++ anonymous-post-board/src/api/api.config.js
@@ -1,4 +1,4 @@
 export const API_URL = "https://api.postboard.martinmiglio.dev";
 
-export const MAX_RETRIES = 2;
-export const REQUEST_TIMEOUT = 1000;
+export const MAX_RETRIES = 4;
+export const REQUEST_TIMEOUT = 5000;
